ec7224a1ae2167d1d4c5579bf61088f62e639c1c
FavioVer <favioa.veron@gmail.com>
2019-11-30 22:49:20 -0300

    Added the use of StringUtils.EMPTY in a class

--- src/main/java/com/travelocity/framework/configuration/BaseConfiguration.java
+++ src/main/java/com/travelocity/framework/configuration/BaseConfiguration.java
@@ -1,11 +1,12 @@
 package com.travelocity.framework.configuration;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.commons.lang3.StringUtils;
 
 public class BaseConfiguration implements Configurable {
 
     @JsonProperty
-    private String baseURL = "";
+    private String baseURL = StringUtils.EMPTY;
 
     @Override
     public String getBaseURL() {
